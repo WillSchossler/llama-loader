8078769a3119c3044d44661c182ede7fd0543cbd
Gabriel Eletronvale <gabriel@eletronvale.com.br>
2026-09-16 16:00:31 -0300

    working test_loader helper, and first test

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -6,7 +6,7 @@ from llama_loader.loader import Loader
 
 
 class Helper:
-    def __init__(self, tmp_path: Path, cli_args: list[str] = ["list"], custom_toml=False) -> None:
+    def __init__(self, tmp_path: Path) -> None:
         self.tmp_path = tmp_path
 
         self.models_dir: Path = tmp_path / "models"
@@ -15,28 +15,32 @@ class Helper:
         self.settings_dir: Path = tmp_path / "settings"
         self.settings_dir.mkdir()
 
+        self.create_configs()
+        self.create_profiles()
+
+    def create_cli_args(self, command: list[str] | None = None) -> Namespace:
         cli = CLI()
-        self.args: Namespace = cli.parser.parse_args(cli_args)
+        command: list[str] = ["list"] if command is None else command
 
-        self.__create_configs_file()
-        self.__create_profiles_file()
-        self.__create_model_files(custom_toml)
+        return cli.parser.parse_args(command)
 
-    def __create_configs_file(self) -> None:
+    def create_configs(self) -> None:
+        self.editor = "code.cmd"
+
         self.browser_path: Path = self.settings_dir / "browser.exe"
         self.browser_path.touch()
 
         self.configs_path: Path = self.settings_dir / "configs.toml"
 
-        toml: str = f"""
+        toml = f"""
         root = '{self.models_dir}'
-        editor = "code.cmd"
+        editor = "{self.editor}"
         browser_path = '{self.browser_path}'
         """
-
+    
         self.configs_path.write_text(toml)
 
-    def __create_profiles_file(self) -> None:
+    def create_profiles(self) -> None:
         self.profiles_path: Path = self.settings_dir / "profiles.toml"
 
         toml: str = """
@@ -50,19 +54,17 @@ class Helper:
         --agent = ""
         """
 
-        self.configs_path.write_text(toml)
+        self.profiles_path.write_text(toml)
 
-    def __create_model_files(self, custom_toml=False) -> None:
-        toml_path: Path = self.models_dir / "models.toml"
 
+    def create_model(self, custom_toml=False):
+        model_path = self.models_dir / "models.toml"
+        
         files: list[str] = ["model.gguf", "template.jinja", "mmproj.gguf", "mtp.gguf"]
 
         for file in files:
             temp_file: Path = self.models_dir / file
             temp_file.touch()
-
-        qwen_model: Path = self.models_dir / "qwen.gguf"
-        qwen_model.touch()
 
         toml: str = (
             custom_toml
@@ -95,25 +97,53 @@ class Helper:
         """
         )
 
-        toml_path.write_text(toml)
+        model_path.write_text(toml)
 
 
 def test_loader(tmp_path, monkeypatch):
-    monkeypatch.setattr(Loader, "SETTINGS_DIR", tmp_path)
+    monkeypatch.setattr(Loader, "SETTINGS_DIR", tmp_path / "settings")
 
-    helper = Helper(tmp_path, ["list"])
-    args = helper.args
+    helper = Helper(tmp_path)
+    helper.create_model()
 
-    Loader(args)
+    args = helper.create_cli_args()
+    loader = Loader(args)
 
-    """assert loader.args.command == args.command
+    assert loader.args.command == args.command
 
-    assert loader.configs.root == tmp_path / "models"
-    assert loader.configs.editor == "code.cmd"
-    assert loader.configs.browser_path == tmp_path / "browser.exe"
+    assert loader.configs.root == helper.models_dir
+    assert loader.configs.editor == helper.editor
+    assert loader.configs.browser_path == helper.browser_path
 
+    assert loader.profiles["default"]["--jinja"] == ""
     assert loader.profiles["default"]["--host"] == "127.0.0.1"
     assert loader.profiles["default"]["--port"] == 9931
-    assert loader.profiles["balanced"]["--temp"] == 0.6
+    
+    assert loader.profiles["balanced"]["--fit"] == "on"
+    assert loader.profiles["balanced"]["--agent"] == ""
 
-    assert loader.models["qwen"].name == "qwen" """
+    assert loader.models["models"].name == "models"
+    assert loader.models["models"].profile == "default"
+
+    assert loader.models["models"].files["--model"] == helper.models_dir / "model.gguf"
+    assert loader.models["models"].files["--mmproj"] == helper.models_dir / "mmproj.gguf"
+    assert loader.models["models"].files["--model-draft"] == helper.models_dir / "mtp.gguf"
+    assert loader.models["models"].files["--chat-template-file"] == helper.models_dir / "template.jinja"
+    
+    assert loader.models["models"].parameters["--spec-type"] == "ngram-mod,draft-mtp"
+    assert loader.models["models"].parameters["--fit"] == "on"
+    assert loader.models["models"].parameters["--jinja"] == ""
+    
+    """
+        [files]
+        --model = 'model.gguf'
+        --mmproj = 'mmproj.gguf'
+        --model-draft = 'mtp.gguf'
+        --chat-template-file = 'template.jinja'
+
+
+        # Additional llama.cpp parameters.
+        [parameters]
+        --spec-type = "ngram-mod,draft-mtp"
+        --fit = "on"
+        --jinja = "" """
